676d2644f7ed296c53bed18d87296f17be979d85
Juan-Gonzalez-Cuadros <55216941+Juan-Gonzalez-Cuadros@users.noreply.github.com>
2022-02-02 13:09:38 -0600

    Update main.dart

--- MiChangarro/mi_changarro/lib/main.dart
+++ MiChangarro/mi_changarro/lib/main.dart
@@ -13,19 +13,70 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Inicio',
+      title: 'Mi Changarro',
       home: Scaffold(
-        body: Container(
-          child: const Align(
-            alignment: Alignment.bottomLeft,
-            child: Text(
-              '¿Sin ganas de salir?',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.deepOrange,
+        body: Align(
+          alignment: Alignment.bottomLeft,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                child: Text(
+                  '¿Sin ganas de salir?',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.deepOrange,
+                  ),
+                ),
               ),
-            ),
+              Container(
+                color: Colors.orange[400], //#FFFF5d39
+                padding: const EdgeInsets.fromLTRB(20, 0, 100, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'No te preocupes. Tenemos una gran selección de productos de tiendas locales. Lo que necesites lo llevamos a tu casa.',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        MaterialButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Inicia Sesión',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          color: Colors.blue[800],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                        ),
+                        MaterialButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Crear Cuenta',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          color: Colors.blue[800],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
