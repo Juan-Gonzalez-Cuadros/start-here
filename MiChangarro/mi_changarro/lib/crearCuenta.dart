2eec809c2900220d92f13edf42b76a4c353a55e8
Juan-Gonzalez-Cuadros <55216941+Juan-Gonzalez-Cuadros@users.noreply.github.com>
2022-02-03 00:36:28 -0600

    Create crearCuenta.dart

---
+++ MiChangarro/mi_changarro/lib/crearCuenta.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class CrearCuenta extends StatelessWidget {
+  const CrearCuenta({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset("lib/img/tcinicio.svg");
+  }
+}
