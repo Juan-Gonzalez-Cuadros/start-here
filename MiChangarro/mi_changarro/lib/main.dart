98c41273f894908473a33cf5371362a5b8a3fcbb
Juan-Gonzalez-Cuadros <55216941+Juan-Gonzalez-Cuadros@users.noreply.github.com>
2022-02-03 17:39:01 -0600

    Update main.dart

--- MiChangarro/mi_changarro/lib/main.dart
+++ MiChangarro/mi_changarro/lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:mi_changarro/crearCuenta.dart';
-import 'package:mi_changarro/iniciarSesion.dart';
+import 'package:mi_changarro/crear_cuenta.dart';
+import 'package:mi_changarro/iniciar_sesion.dart';
 
 // Vista principal de Mi Changarro
 // Conectaa con botones a 'Iniciar Sesion' y 'Crear Cuenta'
@@ -18,74 +19,81 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Mi Changarro',
       home: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        body: Stack(
           children: <Widget>[
-            const Padding(
-              padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
-              child: Text(
-                '¿Sin ganas de salir?',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.deepOrange,
-                ),
-              ),
+            SvgPicture.asset(
+              "lib/img/tcinicio.svg",
+              alignment: Alignment.center,
+              fit: BoxFit.cover,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
             ),
-            Container(
-              color: Colors.orange[400], //#FFFF5d39
-              padding: const EdgeInsets.fromLTRB(20, 0, 100, 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'No te preocupes. Tenemos una gran selección de productos de tiendas locales. Lo que necesites lo llevamos a tu casa.',
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                  child: Text(
+                    '¿Sin ganas de salir?',
                     textAlign: TextAlign.left,
                     style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.white,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.deepOrange,
                     ),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      MaterialButton(
-                        onPressed: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                                builder: (context) => IniciarSesion()),
-                          );
-                        },
-                        child: const Text(
-                          'Inicia Sesión',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        color: Colors.blue[800],
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18),
-                        ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'No te preocupes. Tenemos una gran selección de productos de tiendas locales. Lo que necesites lo llevamos a tu casa.',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
                       ),
-                      MaterialButton(
-                        onPressed: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                                builder: (context) => CrearCuenta()),
-                          );
-                        },
-                        child: const Text(
-                          'Crear Cuenta',
-                          style: TextStyle(color: Colors.white),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        MaterialButton(
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                  builder: (context) => IniciarSesion()),
+                            );
+                          },
+                          child: const Text(
+                            'Inicia Sesión',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          color: Colors.blue[800],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18),
+                          ),
                         ),
-                        color: Colors.blue[800],
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18),
+                        MaterialButton(
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                  builder: (context) => CrearCuenta()),
+                            );
+                          },
+                          child: const Text(
+                            'Crear Cuenta',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          color: Colors.blue[800],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18),
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
             ),
           ],
         ),
