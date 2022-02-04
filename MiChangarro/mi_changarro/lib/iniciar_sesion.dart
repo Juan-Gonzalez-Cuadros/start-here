9fe10fc6bf5a6e96dd524db28445abed3324e202
Juan-Gonzalez-Cuadros <55216941+Juan-Gonzalez-Cuadros@users.noreply.github.com>
2022-02-04 14:31:21 -0600

    Update iniciar_sesion.dart

--- MiChangarro/mi_changarro/lib/iniciar_sesion.dart
+++ MiChangarro/mi_changarro/lib/iniciar_sesion.dart
@@ -9,10 +9,46 @@ class IniciarSesion extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          SvgPicture.asset("lib/img/tcinicsesion.svg"),
-          SvgPicture.asset("lib/img/tclogoblanco.svg"),
+      body: Column(
+        children: [
+          Stack(
+            children: <Widget>[
+              SvgPicture.asset(
+                "lib/img/tcinicsesion.svg",
+                height: 100,
+              ),
+              SvgPicture.asset("lib/img/tclogoblanco.svg"),
+            ],
+          ),
+          const Text(
+            'Inicia sesión',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFFFF5D39),
+            ),
+          ),
+          Column(
+            children: const [
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Nombre completo",
+                  labelStyle: TextStyle(color: Color(0xFF2274A5)),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4.0))),
+                  fillColor: Color(0xFFFFBD2D),
+                  filled: true,
+                ),
+                keyboardType: TextInputType.name,
+                obscureText: true,
+                maxLength: 30,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+            ],
+          ),
         ],
       ),
     );
