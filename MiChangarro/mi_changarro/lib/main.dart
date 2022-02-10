6f054f49e90726ce89f65c83aeeaff38bd913e2c
Juan-Gonzalez-Cuadros <55216941+Juan-Gonzalez-Cuadros@users.noreply.github.com>
2022-02-09 22:28:38 -0600

    Update main.dart

--- MiChangarro/mi_changarro/lib/main.dart
+++ MiChangarro/mi_changarro/lib/main.dart
@@ -20,96 +20,98 @@ class MyApp extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return MaterialApp(
       title: 'Mi Changarro',
-      home: Scaffold(
-        body: SizedBox(
-          height: size.height,
-          width: double.infinity,
-          child: Stack(
-            children: <Widget>[
-              //Imagen svg de fondo
-              Positioned(
-                top: 30,
-                left: 0,
-                child: SvgPicture.asset(
-                  "lib/img/tcinicio.svg",
-                  width: size.width,
+      home: SafeArea(
+        child: Scaffold(
+          body: SizedBox(
+            height: size.height,
+            width: double.infinity,
+            child: Stack(
+              children: <Widget>[
+                //Imagen svg de fondo
+                Positioned(
+                  top: 30,
+                  left: 0,
+                  child: SvgPicture.asset(
+                    "lib/img/tcinicio.svg",
+                    width: size.width,
+                  ),
                 ),
-              ),
-              //Contenido: Titulo, Parrafo y botones
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 40, 60, 60),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    const Align(alignment: Alignment.centerLeft),
-                    const Text(
-                      '¿Sin ganas de salir?',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFFFF5D39),
+                //Contenido: Titulo, Parrafo y botones
+                Container(
+                  padding: const EdgeInsets.fromLTRB(20, 40, 60, 38),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      const Align(alignment: Alignment.centerLeft),
+                      const Text(
+                        '¿Sin ganas de salir?',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFFFF5D39),
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 33),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          'No te preocupes. Tenemos una gran selección de productos de tiendas locales. Lo que necesites lo llevamos a tu casa.',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            fontSize: 19,
-                            color: Colors.white,
+                      const SizedBox(height: 33),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'No te preocupes. Tenemos una gran selección de productos de tiendas locales. Lo que necesites lo llevamos a tu casa.',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontSize: 19,
+                              color: Colors.white,
+                            ),
                           ),
-                        ),
-                        const SizedBox(height: 60),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            MaterialButton(
-                              onPressed: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          const IniciarSesion()),
-                                );
-                              },
-                              child: const Text(
-                                'Inicia Sesión',
-                                style: TextStyle(color: Color(0xFFFFFFFC)),
+                          const SizedBox(height: 60),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              MaterialButton(
+                                onPressed: () {
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const IniciarSesion()),
+                                  );
+                                },
+                                child: const Text(
+                                  'Inicia Sesión',
+                                  style: TextStyle(color: Color(0xFFFFFFFC)),
+                                ),
+                                color: const Color(0xFF2274A5),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18),
+                                ),
                               ),
-                              color: const Color(0xFF2274A5),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18),
+                              MaterialButton(
+                                onPressed: () {
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const CrearCuenta()),
+                                  );
+                                },
+                                child: const Text(
+                                  'Crear Cuenta',
+                                  style: TextStyle(color: Color(0xFFFFFFFC)),
+                                ),
+                                color: const Color(0xFF2274A5),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18),
+                                ),
                               ),
-                            ),
-                            MaterialButton(
-                              onPressed: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          const CrearCuenta()),
-                                );
-                              },
-                              child: const Text(
-                                'Crear Cuenta',
-                                style: TextStyle(color: Color(0xFFFFFFFC)),
-                              ),
-                              color: const Color(0xFF2274A5),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
